de5f839cafa7e42823b89ffa3ba686710a82046e
Adrián <88077103+AdrianSaavedra-GNOSS@users.noreply.github.com>
2021-11-10 10:00:26 +0100

    Update ServicioExterno
    - Agregada gráfica
    - Corrección de errores

---
+++ src/Hercules.MA.ServicioExterno/Hercules.MA.ServicioExterno/Models/DataFechas.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hercules.MA.ServicioExterno.Models.DataFechas
+{
+    public class DataFechas
+    {
+        public int numProyectosInicio { get; set; }
+        public int numProyectosFin { get; set; }
+    }
+}
