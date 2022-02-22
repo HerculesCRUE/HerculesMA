c6c605d3ef5b44ae8dbec7a5adecb2f3b601c251
alvaropalacios-gnoss <60385160+alvaropalacios-gnoss@users.noreply.github.com>
2022-02-22 09:27:48 +0100

    Update ObjectSearch.cs

--- src/Hercules.MA.ServicioExterno/Hercules.MA.ServicioExterno/Models/ObjectSearch.cs
+++ src/Hercules.MA.ServicioExterno/Hercules.MA.ServicioExterno/Models/ObjectSearch.cs
@@ -1,13 +1,198 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Hercules.MA.ServicioExterno.Models
 {
-    public class ObjectSearch
+    public class Person : ObjectSearch
     {
-        public List<string> grupos { get; set; }
-        public List<string> investigadores { get; set; }
-        public List<string> proyectos { get; set; }
-        public List<string> documents { get; set; }
-        public List<string> ros { get; set; }
+        public bool searchable { get; set; }
+
+        public override long Search(string[] pInput)
+        {
+            long respuesta = 0;
+            bool encontrado = true;
+            foreach (string input in pInput)
+            {
+                encontrado = encontrado && titleAuxSearch.Contains(input);
+            }
+            if(encontrado)
+            {
+                respuesta = 1;
+            }
+            return respuesta;
+        }
+    }
+
+    public class Publication : ObjectSearch
+    {
+        public HashSet<string> tagsAuxSearch { get; set; }
+        public string descriptionAuxSearch { get; set; }
+        public HashSet<Person> persons { get; set; }
+
+        public override long Search(string[] pInput)
+        {
+            //Tiene 4 campos (1000,100,10,1)
+            long respuesta = 0;
+            bool encontradoTitulo = true;
+            bool encontradoTags = true;
+            bool encontradoDescripcion = true;
+            bool encontradoAutores = true;
+            foreach (string input in pInput)
+            {
+                encontradoTitulo = encontradoTitulo && titleAuxSearch.Contains(input);
+            }
+
+            foreach (string tag in tagsAuxSearch)
+            {
+                encontradoTags = true;
+                foreach (string input in pInput)
+                {
+                    encontradoTags = encontradoTags && tag.Contains(input);
+                }
+                if (encontradoTags)
+                {
+                    break;
+                }
+            }
+
+            encontradoDescripcion = true;
+            foreach (string input in pInput)
+            {
+                encontradoDescripcion = encontradoDescripcion && descriptionAuxSearch.Contains(input);
+            }
+
+
+            foreach (Person person in persons)
+            {
+                encontradoAutores = true;
+                encontradoAutores = encontradoAutores && person.Search(pInput)>0;
+                if (encontradoAutores)
+                {
+                    break;
+                }
+            }
+
+            if(encontradoTitulo)
+            {
+                respuesta += 1000;
+            }
+            if (encontradoTags)
+            {
+                respuesta += 100;
+            }
+            if (encontradoDescripcion)
+            {
+                respuesta += 10;
+            }
+            if (encontradoAutores)
+            {
+                respuesta += 1;
+            }
+            return respuesta;
+        }
+    }
+
+    public class ResearchObject : ObjectSearch
+    {
+        public HashSet<string> tagsAuxSearch { get; set; }
+        public string descriptionAuxSearch { get; set; }
+        public HashSet<Person> persons { get; set; }
+
+        public override long Search(string[] pInput)
+        {
+            //Tiene 4 campos (1000,100,10,1)
+            long respuesta = 0;
+            bool encontradoTitulo = true;
+            bool encontradoTags = true;
+            bool encontradoDescripcion = true;
+            bool encontradoAutores = true;
+            foreach (string input in pInput)
+            {
+                encontradoTitulo = encontradoTitulo && titleAuxSearch.Contains(input);
+            }
+
+            foreach (string tag in tagsAuxSearch)
+            {
+                encontradoTags = true;
+                foreach (string input in pInput)
+                {
+                    encontradoTags = encontradoTags && tag.Contains(input);
+                }
+                if (encontradoTags)
+                {
+                    break;
+                }
+            }
+
+            encontradoDescripcion = true;
+            foreach (string input in pInput)
+            {
+                encontradoDescripcion = encontradoDescripcion && descriptionAuxSearch.Contains(input);
+            }
+
+            foreach (Person person in persons)
+            {
+                encontradoAutores = true;
+                encontradoAutores = encontradoAutores && person.Search(pInput) > 0;
+                if (encontradoAutores)
+                {
+                    break;
+                }
+            }
+
+            if (encontradoTitulo)
+            {
+                respuesta += 1000;
+            }
+            if (encontradoTags)
+            {
+                respuesta += 100;
+            }
+            if (encontradoDescripcion)
+            {
+                respuesta += 10;
+            }
+            if (encontradoAutores)
+            {
+                respuesta += 1;
+            }
+            return respuesta;
+        }
+    }
+
+    public class Group : ObjectSearch
+    {
+        public string descriptionAuxSearch { get; set; }
+        public HashSet<Person> persons { get; set; }
+
+        public override long Search(string[] pInput)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Project : ObjectSearch
+    {
+        public string descriptionAuxSearch { get; set; }
+        public HashSet<Person> persons { get; set; }
+
+        public override long Search(string[] pInput)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+
+
+    public abstract class ObjectSearch
+    {
+        public string title { get; set; }
+        public string url { get; set; }
+        public string titleAuxSearch { get; set; }
+        public Guid id { get; set; }
+
+        public abstract long Search(string[] pInput);
     }
 }
