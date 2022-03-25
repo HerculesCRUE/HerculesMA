d897e6c8e795b2aac1a6d242ee0c4264bd1347b1
alvaropalacios-gnoss <60385160+alvaropalacios-gnoss@users.noreply.github.com>
2022-03-25 08:37:00 +0100

    Create ObjectSearch.cs

---
+++ src/Hercules.MA.ServicioExterno/Hercules.MA.ServicioExterno/Models/Buscador/ObjectSearch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Hercules.MA.ServicioExterno.Models.Buscador
+{    
+    public abstract class ObjectSearch
+    {
+        public string title { get; set; }
+        public string url { get; set; }
+        public string titleAuxSearch { get; set; }
+        public Guid id { get; set; }
+        public abstract long Search(string[] pInput);
+    }
+}
