4e7e1fc49cd3900c9b4a160bf46e56c117d312b7
alvaropalacios-gnoss <60385160+alvaropalacios-gnoss@users.noreply.github.com>
2022-03-25 08:37:02 +0100

    Create Person.cs

---
+++ src/Hercules.MA.ServicioExterno/Hercules.MA.ServicioExterno/Models/Buscador/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Hercules.MA.ServicioExterno.Models.Buscador
+{
+    public class Person : ObjectSearch
+    {
+        public bool searchable { get; set; }
+        
+        /**
+         * Método para buscar dentro de Person
+         */
+        public override long Search(string[] pInput)
+        {
+            long respuesta = 0;
+            bool encontradoTitulo = true;
+            // Busca si TODAS las palabas se encuentran en el título
+            foreach (string input in pInput)
+            {
+                encontradoTitulo = encontradoTitulo && titleAuxSearch.Contains(input);
+            }
+            // Resultados
+            if (encontradoTitulo)
+            {
+                respuesta += 1000;
+            }
+            return respuesta;
+        }
+    }
+
+}
