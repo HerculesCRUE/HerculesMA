dfa65eb1e2f186c520fc4825c4f4ef57d8eec6ed
alvaropalacios-gnoss <60385160+alvaropalacios-gnoss@users.noreply.github.com>
2022-03-30 10:11:18 +0200

    Update ObjectSearch.cs

--- src/Hercules.MA.ServicioExterno/Hercules.MA.ServicioExterno/Models/Buscador/ObjectSearch.cs
+++ src/Hercules.MA.ServicioExterno/Hercules.MA.ServicioExterno/Models/Buscador/ObjectSearch.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace Hercules.MA.ServicioExterno.Models.Buscador
@@ -8,8 +9,34 @@ namespace Hercules.MA.ServicioExterno.Models.Buscador
     {
         public string title { get; set; }
         public string url { get; set; }
-        public string titleAuxSearch { get; set; }
+        public HashSet<string> titleAuxSearch { get; set; }
         public Guid id { get; set; }
-        public abstract long Search(string[] pInput);
+
+        /// <summary>
+        /// Método de búsqueda para el autocompletar
+        /// </summary>
+        /// <param name="pInput">Texto a buscar (excepto la última palabra)</param>
+        /// <param name="pLastInput">Última palabra del texto a buscar</param>
+        /// <returns>Peso</returns>
+        public abstract long SearchAutocompletar(HashSet<string> pInput, string pLastInput);
+
+        /// <summary>
+        /// Método de búsqueda para determinar si la búsqueda ofrecería resultados
+        /// </summary>
+        /// <param name="pInput">Texto a buscar (excepto la última palabra)</param>
+        /// <param name="pLastInput">Última palabra del texto a buscar</param>
+        /// <returns>Peso</returns>
+        public abstract bool SearchBuscador(HashSet<string> pInput, string pLastInput);
+
+
+        public bool SearchForAutocomplete(HashSet<string> pText,HashSet<string> pInput,string pLastInput)
+        {            
+            return pText.IsSupersetOf(pInput) && pText.Any(x => x.StartsWith(pLastInput));
+        }
+
+        public bool SearchForSearcher(HashSet<string> pText, HashSet<string> pInput, string pLastInput)
+        {
+            return pText.IsSupersetOf(pInput) && pText.Contains(pLastInput);
+        }
     }
 }
