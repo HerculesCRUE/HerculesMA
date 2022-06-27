a6f9d08699b905bfaa1f2147ee0d0fa60e3e339c
Adrián <88077103+AdrianSaavedra-GNOSS@users.noreply.github.com>
2022-06-27 09:15:02 +0200

    Cambio del GraphicEngine
    - Pasado de ED -> MA

---
+++ src/Hercules.MA.GraphicEngine/Hercules.MA.GraphicEngine/Models/Graficas/DataGraphicUser.cs
@@ -0,0 +1,13 @@
+﻿namespace Hercules.MA.GraphicEngine.Models.Graficas
+{
+    public class DataGraphicUser
+    {
+        public string titulo { get; set; }
+        public int orden { get; set; }
+        public string idPagina { get; set; }
+        public string idGrafica { get; set; }
+        public string filtro { get; set; }
+        public string anchura { get; set; }
+        public string idRecurso { get; set; }
+    }
+}
---
+++ src/Hercules.MA.GraphicEngine/Hercules.MA.GraphicEngine/Models/Graficas/GraficaBase.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace Hercules.MA.GraphicEngine.Models.Graficas
+{
+    public abstract class GraficaBase
+    {
+        public abstract byte[] GenerateCSV();
+        public string groupId { get; set; }
+        public bool isAbr { get; set; }
+        public bool isPercentage { get; set; }
+        public bool isDate { get; set; }
+        public bool isHorizontal { get; set; }
+        public bool isVertical { get; set; }
+        public bool isNodes { get; set; }
+    }
+
+    public class Options
+    {
+        public Animation animation { get; set; }
+        public Plugin plugins { get; set; }
+        public Dictionary<string, Eje> scales { get; set; }
+        public string indexAxis { get; set; }
+    }
+
+    public class Animation
+    {
+        public int duration { get; set; }
+    }
+
+    public class Plugin
+    {
+        public Title title { get; set; }
+    }
+
+    public class Title
+    {
+        public bool display { get; set; }
+        public string text { get; set; }
+    }
+
+    public class Eje
+    {
+        public string position { get; set; }
+        public Title title { get; set; }
+    }
+}
