d21f056c7a09753d07434dc3ed28b06b4638204a
alvaropalacios-gnoss <60385160+alvaropalacios-gnoss@users.noreply.github.com>
2022-06-28 13:21:56 +0200

    Create EnrichmentSimilarityGet.cs

---
+++ src/Hercules.MA.ServicioExterno/Hercules.MA.ServicioExterno/Models/Similarity/EnrichmentSimilarityGet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hercules.MA.ServicioExterno.Models.Similarity
+{
+    public class EnrichmentSimilarityGet
+    {
+        public string ro_id { get; set; }
+        public string ro_type_target { get; set; }
+    }
+}
