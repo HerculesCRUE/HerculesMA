68d6a99a5f3186729a325770157f08a864c271be
alvaropalacios-gnoss <60385160+alvaropalacios-gnoss@users.noreply.github.com>
2022-06-29 08:27:22 +0200

    Update AccionesSimilarity.cs

--- src/Hercules.MA.ServicioExterno/Hercules.MA.ServicioExterno/Controllers/Acciones/AccionesSimilarity.cs
+++ src/Hercules.MA.ServicioExterno/Hercules.MA.ServicioExterno/Controllers/Acciones/AccionesSimilarity.cs
@@ -29,17 +29,40 @@ namespace Hercules.MA.ServicioExterno.Controllers.Acciones
         private static ResourceApi mResourceApi = new ResourceApi(RUTA_OAUTH);
         #endregion
 
-        public List<string> GetSimilaritiesDocument(string pIdDocument, ConfigService pConfig)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pId">ID del elemento al que buscar similares</param>
+        /// <param name="pConfig">Config</param>
+        /// <param name="pType">Tipo: 'research_paper' o 'code_project'</param>
+        /// <returns></returns>
+        public Dictionary<string, Dictionary<string, float>> GetSimilarities(string pId, ConfigService pConfig, string pType)
         {
-            List<string> listID = new List<string>();
+            Dictionary<string, Dictionary<string, float>> dicSimilars = new Dictionary<string, Dictionary<string, float>>();
+
+            string rdfType = "";
+            string graph = "";
+            switch (pType)
+            {
+                case "research_paper":
+                    rdfType = "http://purl.org/ontology/bibo/Document";
+                    graph = "";
+                    break;
+                case "code_project":
+                    rdfType = "http://w3id.org/roh/ResearchObject";
+                    graph = "researchobject";
+                    break;
+                default:
+                    throw new Exception("El tipo " + pType + " no está permitido para buscar similitudes, sólo está permitido 'research_paper' o 'code_project'");
+            }
 
             // Cliente.
             HttpClient client = new HttpClient();
             client.Timeout = TimeSpan.FromDays(1);
 
             EnrichmentSimilarityGet enrichmentSimilarityGet = new EnrichmentSimilarityGet();
-            enrichmentSimilarityGet.ro_id = pIdDocument;
-            enrichmentSimilarityGet.ro_type_target = "research_paper";
+            enrichmentSimilarityGet.ro_id = pId;
+            enrichmentSimilarityGet.ro_type_target = pType;
 
             // Conversión de los datos.
             string informacion = JsonConvert.SerializeObject(enrichmentSimilarityGet);
@@ -49,32 +72,32 @@ namespace Hercules.MA.ServicioExterno.Controllers.Acciones
             if (responsePost.IsSuccessStatusCode)
             {
                 EnrichmentSimilarityGetResponse responseGetObject = responsePost.Content.ReadAsAsync<EnrichmentSimilarityGetResponse>().Result;
-                listID = responseGetObject.similar_ros_calculado.Keys.ToList();
-                listID.Remove(pIdDocument);
+                dicSimilars = responseGetObject.similar_ros_calculado;
+                dicSimilars.Remove(pId);
             }
-            if (listID.Count > 0)
+            if (dicSimilars.Count > 0)
             {
-                string select = "select distinct ?doc from <http://gnoss.com/curriculumvitae.owl> ";
+                //Hacemos una verificación para que sólo se devuelvan validados o publicios en el cv
+                string select = "select distinct ?id from <http://gnoss.com/curriculumvitae.owl> ";
                 string where = $@"
 where{{
-    FILTER(?doc in (<{string.Join(">,<", listID)}>))
-    ?doc a <http://purl.org/ontology/bibo/Document>.
-    {{ ?doc <http://w3id.org/roh/isValidated> 'true'.}}
+    FILTER(?id in (<{string.Join(">,<", dicSimilars.Keys)}>))
+    ?id a <{rdfType}>.
+    {{ ?id <http://w3id.org/roh/isValidated> 'true'.}}
     UNION
     {{  
         ?cv a <http://w3id.org/roh/CV>.
         ?cv ?p1 ?o1.
         ?o1 ?p2 ?item.
-        ?item <http://vivoweb.org/ontology/core#relatedBy> ?doc.
+        ?item <http://vivoweb.org/ontology/core#relatedBy> ?id.
         ?item <http://w3id.org/roh/isPublic> 'true'.
     }}
 
 }}";
-                listID = listID.Intersect(mResourceApi.VirtuosoQuery(select, where, "document").results.bindings.Select(x => x["doc"].value)).ToList();
-                listID = listID.Select(x => mResourceApi.GetShortGuid(x).ToString()).ToList();
+                List<string> listID = mResourceApi.VirtuosoQuery(select, where, graph).results.bindings.Select(x => x["doc"].value).ToList();
+                dicSimilars = dicSimilars.Where(x => listID.Contains(x.Key)).ToDictionary(x => x.Key, x => x.Value);
             }
-            return listID;
+            return dicSimilars;
         }
-
     }
 }
