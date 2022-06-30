b0f0112d8182df3dd825b05c17c143e8a1fc7c6b
alvaropalacios-gnoss <60385160+alvaropalacios-gnoss@users.noreply.github.com>
2022-06-30 10:25:46 +0200

    Update AccionesSimilarity.cs

--- src/Hercules.MA.ServicioExterno/Hercules.MA.ServicioExterno/Controllers/Acciones/AccionesSimilarity.cs
+++ src/Hercules.MA.ServicioExterno/Hercules.MA.ServicioExterno/Controllers/Acciones/AccionesSimilarity.cs
@@ -38,68 +38,8 @@ namespace Hercules.MA.ServicioExterno.Controllers.Acciones
         /// <returns></returns>
         public List<KeyValuePair<Guid, Dictionary<string, float>>> GetSimilarities(string pId, ConfigService pConfig, string pType)
         {
-            Dictionary<string, Dictionary<string, float>> dicSimilarsAux = new Dictionary<string, Dictionary<string, float>>();
-
-            string rdfType = "";
-            string graph = "";
-            switch (pType)
-            {
-                case "research_paper":
-                    rdfType = "http://purl.org/ontology/bibo/Document";
-                    graph = "document";
-                    break;
-                case "code_project":
-                    rdfType = "http://w3id.org/roh/ResearchObject";
-                    graph = "researchobject";
-                    break;
-                default:
-                    throw new Exception("El tipo " + pType + " no está permitido para buscar similitudes, sólo está permitido 'research_paper' o 'code_project'");
-            }
-
-            // Cliente.
-            HttpClient client = new HttpClient();
-            client.Timeout = TimeSpan.FromDays(1);
-
-            EnrichmentSimilarityGet enrichmentSimilarityGet = new EnrichmentSimilarityGet();
-            enrichmentSimilarityGet.ro_id = pId;
-            enrichmentSimilarityGet.ro_type_target = pType;
-
-            // Conversión de los datos.
-            string informacion = JsonConvert.SerializeObject(enrichmentSimilarityGet);
-            StringContent contentData = new StringContent(informacion, System.Text.Encoding.UTF8, "application/json");
-
-            var responsePost = client.PostAsync(pConfig.GetUrlSimilarity(), contentData).Result;
-            if (responsePost.IsSuccessStatusCode)
-            {
-                EnrichmentSimilarityGetResponse responseGetObject = responsePost.Content.ReadAsAsync<EnrichmentSimilarityGetResponse>().Result;
-                dicSimilarsAux = responseGetObject.similar_ros_calculado;
-                dicSimilarsAux.Remove(pId);
-            }
-            if (dicSimilarsAux.Count > 0)
-            {
-                //Hacemos una verificación para que sólo se devuelvan validados o publicios en el cv
-                string select = "select distinct ?id from <http://gnoss.com/curriculumvitae.owl> ";
-                string where = $@"
-where{{
-    FILTER(?id in (<{string.Join(">,<", dicSimilarsAux.Keys)}>))
-    ?id a <{rdfType}>.
-    {{ ?id <http://w3id.org/roh/isValidated> 'true'.}}
-    UNION
-    {{  
-        ?cv a <http://w3id.org/roh/CV>.
-        ?cv ?p1 ?o1.
-        ?o1 ?p2 ?item.
-        ?item <http://vivoweb.org/ontology/core#relatedBy> ?id.
-        ?item <http://w3id.org/roh/isPublic> 'true'.
-    }}
-
-}}";
-                List<string> listID = mResourceApi.VirtuosoQuery(select, where, graph).results.bindings.Select(x => x["id"].value).ToList();
-                dicSimilarsAux = dicSimilarsAux.Where(x => listID.Contains(x.Key)).ToDictionary(x => x.Key, x => x.Value);
-            }
-            List<KeyValuePair<Guid, Dictionary<string, float>>> dicSimilars = dicSimilarsAux.ToDictionary(x=>mResourceApi.GetShortGuid(x.Key),x=>x.Value).ToList();
-
-            return dicSimilars;
+            UtilsSimilarity utilsSimilarity = new UtilsSimilarity(pConfig.GetUrlSimilarity(), mResourceApi, pType);
+            return utilsSimilarity.GetSimilars(pId);
         }
     }
 }
