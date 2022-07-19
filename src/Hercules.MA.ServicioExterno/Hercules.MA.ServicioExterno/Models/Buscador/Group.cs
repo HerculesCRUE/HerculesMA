1f77a7e0d3db665ce34de40bac48468eae0219d7
alvaropalacios-gnoss <60385160+alvaropalacios-gnoss@users.noreply.github.com>
2022-07-19 13:25:51 +0200

    Update Group.cs

--- src/Hercules.MA.ServicioExterno/Hercules.MA.ServicioExterno/Models/Buscador/Group.cs
+++ src/Hercules.MA.ServicioExterno/Hercules.MA.ServicioExterno/Models/Buscador/Group.cs
@@ -8,51 +8,9 @@ namespace Hercules.MA.ServicioExterno.Models.Buscador
 {
     public class Group : ObjectSearch
     {
-        public HashSet<string> descriptionAuxSearch { get; set; }
-        public HashSet<Person> persons { get; set; }
+        //Titulo-->10000
+        //Descripción-->1000
+        //Autor-->1
 
-
-        public override long SearchAutocompletar(HashSet<string> pInput, string pLastInput)
-        {
-            long respuestaPeso = 0;
-            bool encontradoTitulo = SearchForAutocomplete(titleAuxSearch, pInput, pLastInput);
-            bool encontradoDescripcion = SearchForAutocomplete(descriptionAuxSearch, pInput, pLastInput);
-            int numAutores = 0;
-
-            foreach (Person person in persons)
-            {
-                if (person.SearchAutocompletar(pInput, pLastInput) > 0)
-                {
-                    numAutores++;
-                }
-            }
-
-            // Resultados
-            if (encontradoTitulo)
-            {
-                respuestaPeso += 10000;
-            }
-
-            if (encontradoDescripcion)
-            {
-                respuestaPeso += 1000;
-            }
-
-            respuestaPeso += numAutores;
-
-            return respuestaPeso;
-        }
-
-        public override bool SearchBuscador(HashSet<string> pInput, string pLastInput)
-        {
-            bool respuesta = false;
-            respuesta = respuesta || SearchForSearcher(titleAuxSearch, pInput, pLastInput);
-            respuesta = respuesta || SearchForSearcher(descriptionAuxSearch, pInput, pLastInput);
-            foreach (Person person in persons)
-            {
-                respuesta = respuesta || person.SearchBuscador(pInput, pLastInput);
-            }
-            return respuesta;
-        }
     }
 }
