f7f535528fdda9f9ad200d493ec7c4eabfa0c7e7
Adrián <88077103+AdrianSaavedra-GNOSS@users.noreply.github.com>
2022-08-03 14:50:17 +0200

    Update ServicioExterno
    - Preparación para la carga de la publicación mediante DOI.

--- src/Hercules.MA.ServicioExterno/Hercules.MA.ServicioExterno/Controllers/ConfigService.cs
+++ src/Hercules.MA.ServicioExterno/Hercules.MA.ServicioExterno/Controllers/ConfigService.cs
@@ -11,7 +11,9 @@ namespace Hercules.MA.ServicioExterno.Controllers
         public static IConfigurationRoot configuracion;
         private string RabbitConnectionString { get; set; }
         private string QueueRabbit { get; set; }
+        private string DoiQueueRabbit { get; set; }
         private string UrlSimilarity { get; set; }
+        private string UrlPublicacion { get; set; }
 
         /// <summary>
         /// Constructor.
@@ -81,6 +83,29 @@ namespace Hercules.MA.ServicioExterno.Controllers
         }
 
         /// <summary>
+        /// Obtiene la el nombre de la cola Rabbit de configuración.
+        /// </summary>
+        /// <returns>Nombre de la cola Rabbit.</returns>
+        public string GetDoiQueueRabbit()
+        {
+            if (string.IsNullOrEmpty(DoiQueueRabbit))
+            {
+                IDictionary environmentVariables = Environment.GetEnvironmentVariables();
+                string queue = "";
+                if (environmentVariables.Contains("DoiQueueRabbit"))
+                {
+                    queue = environmentVariables["DoiQueueRabbit"] as string;
+                }
+                else
+                {
+                    queue = configuracion["DoiQueueRabbit"];
+                }
+                DoiQueueRabbit = queue;
+            }
+            return DoiQueueRabbit;
+        }
+
+        /// <summary>
         /// Obtiene la URL del servicio de similitud
         /// </summary>
         /// <returns>Url del servicio</returns>
@@ -101,6 +126,29 @@ namespace Hercules.MA.ServicioExterno.Controllers
                 UrlSimilarity = url;
             }
             return UrlSimilarity;
+        }
+
+        /// <summary>
+        /// Obtiene la URL del servicio de similitud
+        /// </summary>
+        /// <returns>Url del servicio</returns>
+        public string GetUrlPublicacion()
+        {
+            if (string.IsNullOrEmpty(UrlPublicacion))
+            {
+                IDictionary environmentVariables = Environment.GetEnvironmentVariables();
+                string url = "";
+                if (environmentVariables.Contains("UrlPublicacion"))
+                {
+                    url = environmentVariables["UrlPublicacion"] as string;
+                }
+                else
+                {
+                    url = configuracion["UrlPublicacion"];
+                }
+                UrlPublicacion = url;
+            }
+            return UrlPublicacion;
         }
     }
 }
--- src/Hercules.MA.ServicioExterno/Hercules.MA.ServicioExterno/Controllers/FuentesExternasController.cs
+++ src/Hercules.MA.ServicioExterno/Hercules.MA.ServicioExterno/Controllers/FuentesExternasController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Hercules.MA.ServicioExterno.Controllers
 {
@@ -70,6 +72,76 @@ namespace Hercules.MA.ServicioExterno.Controllers
             }
 
             return Ok();
+        }
+
+        [HttpGet("InsertDoiToQueue")]
+        public bool InsertDoiToQueueFuentesExternas(string pDoi, string pNombreCompletoAutor, string pOrcid)
+        {
+            try
+            {
+                ReadRabbitService rabbitMQService = new ReadRabbitService(_Configuracion);
+
+                // De momento, el DOI, ORCID y el nombre del autor son obligatorios. TODO: ¿ORCID en un futuro no va a ser obligatorio?
+                if (!string.IsNullOrEmpty(pDoi) && !string.IsNullOrEmpty(pNombreCompletoAutor) && !string.IsNullOrEmpty(pOrcid))
+                {
+                    // Comprobación si el documento es válido o no. Para ello, hay que procesar dicho fichero para ver si corresponde algún autor al pasado por parámetro.
+                    // Aunque se haga la petición, no tarda. TODO: ¿Posible manera de mejorar esto?
+                    Uri url = new Uri(string.Format(_Configuracion.GetUrlPublicacion() + "GetRoPublication?pDoi={0}&pNombreCompletoAutor={1}", pDoi, pNombreCompletoAutor));
+                    string info_publication = httpCall(url.ToString(), "GET", new Dictionary<string, string>()).Result;
+                    if (string.IsNullOrEmpty(info_publication) || info_publication.Trim() == "[]")
+                    {
+                        return false;
+                    }
+
+                    // Inserción a la cola.
+                    List<string> listaDatos = new List<string>() { pDoi, pNombreCompletoAutor, pOrcid };
+                    rabbitMQService.PublishMessage(listaDatos, _Configuracion.GetDoiQueueRabbit());
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// A Http calls function.
+        /// </summary>
+        /// <param name="url">The http call URL.</param>
+        /// <param name="method">Crud method for the call.</param>
+        /// <returns></returns>
+        protected async Task<string> httpCall(string url, string method = "GET", Dictionary<string, string> headers = null)
+        {
+            HttpResponseMessage response;
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.Timeout = TimeSpan.FromHours(24);
+                using (var request = new HttpRequestMessage(new HttpMethod(method), url))
+                {
+                    request.Headers.TryAddWithoutValidation("Accept", "application/json");
+
+                    if (headers != null && headers.Count > 0)
+                    {
+                        foreach (var item in headers)
+                        {
+                            request.Headers.TryAddWithoutValidation(item.Key, item.Value);
+                        }
+                    }
+                    response = await httpClient.SendAsync(request);
+                }
+            }
+
+            if (response.Content != null)
+            {
+                return await response.Content.ReadAsStringAsync();
+            }
+
+            else
+            {
+                return string.Empty;
+            }
         }
     }
 }
